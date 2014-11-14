11b68d60e915c2ec861fb9cf095da8a523214810
Drew Zawora <drewzawora@yahoo.com>
2014-11-14 16:12:51 +0000

    first

---
+++ arithmetic.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+// function add($a, $b) {
+// 	echo $a + $b . PHP_EOL;
+// }
+
+// add(5, 4);
+
+
+
+
+
+function add($a, $b) {
+    echo $a + $b . PHP_EOL;
+}
+
+	add(10, 5);
+
+function subtract($a, $b) {
+   	echo $a - $b . PHP_EOL;
+}
+
+	subtract(20, 7);
+
+function multiply($a, $b) {
+    echo $a * $b . PHP_EOL;
+}
+
+	multiply(20, 5);
+
+function divide($a, $b) {
+    echo $a / $b . PHP_EOL;
+}
+
+	divide(100, 5);
+
+function modulus($a, $b){
+		echo $a % $b . PHP_EOL;
+}
+
+	modulus(3, 17);
