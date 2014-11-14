75a6be8f87cfbdb4144b2f242c86ffeb3b3136b9
andrew zawora <andrewzawora@andrews-MacBook-Pro.local>
2014-11-14 10:23:28 -0600

    Numbers

---
+++ numbers.php
@@ -0,0 +1,20 @@
+<?php
+
+$numbers = [1, 2, 3, 4, 5];
+
+foreach ($numbers as $key => $value) {
+	echo ("\$value has a key of {$key} and a value of {$value}\n");
+}
+
+echo $numbers[2];
+
+$studentDetails = array(
+		'name' => 'Jane Doe',
+		'age' => 29
+		);
+
+foreach ($studentDetails as $key => $value) {
+	echo "Student's $key is $value\n";
+}
+
+
